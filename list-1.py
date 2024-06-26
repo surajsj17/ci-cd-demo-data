1b5447ecd5ce268a10940b70285e9dbc3ab6449b
Suraj Jaiswal <120768713+surajsj17@users.noreply.github.com>
2024-06-26 16:21:20 +0530

    Add files via upload

---
+++ list-1.py
@@ -0,0 +1,16 @@
+# # write the program to check the entered employee name is existing or not 
+# emp_name = input("Enter the name : ")
+name = ["jenny","bob","virat","rohit"]
+if emp_name in name:
+    print ("The Employee name is Matching : " ,emp_name)
+else:
+    print ("The Employee name is Not Matching : " ,emp_name)
+'''
+OUTPUT = Enter the name : virat
+         The Employee name is Matching :  virat
+'''    
+
+    
+
+
+
